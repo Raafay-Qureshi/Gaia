e2a9bc591d5cf9bf22a4a6d0a3597ee0ec1512db
Laavanya Thiagalingam <laavanya.thiaga@gmail.com>
2022-08-04 21:12:49 -0400

    dry

--- Gaia/ScanViewController.swift
+++ Gaia/ScanViewController.swift
@@ -12,16 +12,70 @@ import Vision
 
 class ScanViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    override var prefersStatusBarHidden: Bool {
+    @IBOutlet weak var accLabel: UILabel!
+    @IBOutlet weak var objectNameLabel: UILabel!
+    @IBOutlet weak var belowView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+        //camera starts
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        view.addSubview(belowView)
+        
+        belowView.clipsToBounds = true
+        belowView.layer.cornerRadius = 15.0
+        belowView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+     
+      //  VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //print("Camera was able to capture a frame", Date())
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return}
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {return}
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            //perhaps check the err
+            print(finishedReq.results!)
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
+
+            guard let firstObservation = results.first else {return}
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            self.objectNameLabel.text = firstObservation.identifier
+            self.accLabel.text = ("\(firstObservation.confidence)")
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        
+    }
+    /*override var prefersStatusBarHidden: Bool {
         return true
     }
     
-    
+    @IBOutlet weak var accLabel: UILabel!
     @IBOutlet weak var belowView: UIView!
     @IBOutlet weak var objectNameLabel: UILabel!
-    @IBOutlet weak var accuracyLabel: UILabel!
     
-    
+    //let config = MLModelConfiguration().self
     var model = Resnet50().model
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,13 +120,13 @@ class ScanViewController: UIViewController, AVCaptureVideoDataOutputSampleBuffer
             
             DispatchQueue.main.async {
                 self.objectNameLabel.text = name
-                self.accuracyLabel.text = "Accuracy: \(acc)%"
+                self.accLabel.text = "Accuracy: \(acc)%"
             }
             
         }
         
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
         
-    }
+    } */
 }
 
